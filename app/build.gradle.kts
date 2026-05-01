cb4e729f78fc5a3d680eb3a3e87836150836d87e
red-cap <luisribeirorosa@gmail.com>
2026-05-01 17:29:12 +0200

    Bump version to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mysimplemeditation.app"
         minSdk = 31
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.2"
+        versionCode = 5
+        versionName = "1.1.3"
     }
 
     buildTypes {
