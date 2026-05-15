9add825e181048aa7119bca6b077830b06dafccb
red-cap <luisribeirorosa@gmail.com>
2026-05-15 09:38:23 +0200

    increase version to 1.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mysimplemeditation.app"
         minSdk = 31
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.1.5"
+        versionCode = 8
+        versionName = "1.1.6"
     }
 
     buildTypes {
